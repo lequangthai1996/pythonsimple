c1360a10780e5c30952e2385268f4707e8be812d
at-thaile <thai.le@asiantech.vn>
2019-01-04 09:30:15 +0700

    date2

---
+++ boards/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+from .views import home
+# Create your tests here.
+
+class HomeTest(TestCase):
+  def test_home_view_status_code(self):
+    url = reverse('home')
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, 200)
+
+  def test_home_url_resolve_homeview(self):
+    view = resolve('/')
+    self.assertEquals(view.func, home)
